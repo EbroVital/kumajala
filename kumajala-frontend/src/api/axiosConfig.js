c016ce174a5ddd2015a10a917a6fe73dd17ab16b
N.E.V.E <167760329+EbroVital@users.noreply.github.com>
2025-11-15 13:40:14 +0100

    Update axiosConfig.js
    fix : add url for backend

--- kumajala-frontend/src/api/axiosConfig.js
+++ kumajala-frontend/src/api/axiosConfig.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-// Définir l'URL de base de votre backend Flask
-// Si votre backend Flask tourne sur localhost:5000 et que votre frontend est sur un port différent,
-// vous devrez utiliser l'URL complète ici.
-// Assurez-vous que cette URL correspond à celle de votre backend.
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://kumajala-backend.onrender.com/kumajala-api/v1'; // Ajustez si votre base URL est différente
+// URL de production du backend
+const API_BASE_URL = 'https://kumajala-backend.onrender.com/kumajala-api/v1';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
@@ -13,16 +10,12 @@ const apiClient = axios.create({
   },
 });
 
-// Vous pouvez ajouter des intercepteurs ici si nécessaire, par exemple pour gérer les erreurs
 apiClient.interceptors.response.use(
   response => response,
   error => {
     console.error('Erreur de l\'API:', error.response || error.message);
-    // Vous pouvez ajouter une logique de gestion d'erreur globale ici,
-    // comme afficher une notification à l'utilisateur ou rediriger.
     return Promise.reject(error);
   }
 );
 
 export default apiClient;
-
